092fc872843d2f5327dab7087dc33cfb76a52382
Gokul S Krishnan <gsk1692@gmail.com>
2020-04-05 23:14:59 +0530

    added hale lab new trial web

---
+++ halelab/js/posts.js
@@ -0,0 +1,29 @@
+const posts = [
+	{
+		"title" : "ICDCN 2019 @ IISc Bangalore",
+		"date" : "January 4, 2019",
+		"body" : "We presented our paper titled <i><a href='https://dl.acm.org/citation.cfm?id=3297118' target='_blank'>'Evaluating the Quality of Word Representation Models for Unstructured Clinical Text based ICU Mortality Prediction'</a></i>, authored by Gokul S Krishnan & Dr. Sowmya Kamath S, at the Cloud based Health Informatics (CHIN 2019) colocated with the 20th International Conference on Distributed Computing and Networking (ICDCN '19) at IISc Bangalore, Karnataka, India on 4th January 2019. The presentation was done by HALE Lab @ NITK Research Scholar, Gokul S Krishnan.",
+		"images" : ["./images/icdcn2019/1.jpg", "./images/icdcn2019/2.jpg"]
+	},
+
+	{
+		"title" : "ICIIS 2018 @ IIT Ropar",
+		"date" : "December 1, 2018",
+		"body" : "We presented our paper titled <i><a href='https://ieeexplore.ieee.org/abstract/document/8721432/' target='_blank'>'A Hybrid Feature Modeling Approach for Content-Based Medical Image Retrieval'</a></i>, authored by Karthik K & Dr. Sowmya Kamath S, in the 13th IEEE International Conference on Industrial and Information Systems (ICIIS 2018) at IIT Ropar, Punjab, India on Dec 2018. The poster presentation was done by HALE Lab @ NITK Research Scholar, Karthik K.",
+		"images" : ["./images/iciis2018/1.jpg", "./images/iciis2018/2.jpg"]
+	},
+
+	{
+		"title" : "NLDB 2018 @ Paris",
+		"date" : "June 22, 2018",
+		"body" : "We presented our paper titled <i>'A Supervised Learning Approach for ICU Mortality Prediction Based on Unstructured Electrocardiogram Text Reports'</i>, authored by Gokul S Krishnan & Dr. Sowmya Kamath S, in the 23rd International Conference on Natural Language and Information Systems (NLDB 2018) at Paris, France on 15th June 2018. The presentation was done by HALE Lab @ NITK Research Scholar, Gokul S Krishnan.",
+		"images" : ["./images/nldb2018/nldb2.jpg", "./images/nldb2018/nldb3.jpg"]
+	},
+
+	{
+		"title" : "HADM 2018 @ New Delhi",
+		"date" : "March 11, 2018",
+		"body" : "Representing HALE Lab @ NITK, Gokul S Krishnan attended a 2-day symposium <i>'Health Analytics and Disease Modeling (HADM) 2018'</i> at the National Academy of Medical Sciences, AIMS, New Delhi, India. The event was held jointly by National Institute of Medical Statistics (NIMS) - Indian Council of Medical Research (ICMR) and Public Health Dynamics Laboratory, University of Pittsburgh, USA. The talk covered various topics on Healthcare Informatics and Population Health. Speakers included Directors and other senior officials from NIMS, ICMR and NCDIR Bengaluru and also prominent researchers in the domain from University of Pittsburgh.",
+		"images" : ["./images/hadm2018/hadm1.jpg", "./images/hadm2018/hadm4.jpg", "./images/hadm2018/hadm3.jpg", "./images/hadm2018/hadm2.jpg"]
+	}
+]
